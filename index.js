9ed0235f05bd0d7394fba4f923d95125748384c0
John Robertson <tiktaalikdesign@gmail.com>
2020-03-22 17:47:50 -0400

    checkpoint before adding css framework

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;//Loads the handlebars module
+const port = process.env.PORT || 3000;//Loads the handlebars module
 const handlebars = require('express-handlebars');//Sets our app to use the handlebars engine
 //instead of app.set('view engine', 'handlebars'); 
 app.set('view engine', 'hbs');//instead of app.engine('handlebars', handlebars({
+
 app.engine('hbs', handlebars({
-layoutsDir: __dirname + '/views/layouts',
-//new configuration parameter
-extname: 'hbs',
-defaultLayout: 'default',
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'default',
 }));
+
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-fakeApi = () => 'Faker';
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-console.log(`Rendered root.`)
-res.render('main', {layout : 'index', proPlayer: fakeApi()});
-});
+    fakeApi = () => 'Faker'; // API Call Here :)
+    console.log(`Rendered root and fake API call.`);
+  
+        res.render('main', { layout: 'index', proPlayer: fakeApi() });
+    
+})
+
+app.get('/new', (req, res) => {
+    res.render('new', { layout: 'index'} )
+})
+
 app.listen(port, () => console.log(`App listening to port ${port}`));
