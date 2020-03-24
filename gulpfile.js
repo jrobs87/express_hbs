810ba5adc0410d06c6414408a77c1041d11fc563
John Robertson <tiktaalikdesign@gmail.com>
2020-03-24 00:39:55 -0400

    checkpoint before refactoring build tools. moving browsersync away from package.json into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,38 @@
 
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css', function () {
+    return gulp.src('src/*.css') // ----- declare directory 
+        .pipe(concatCss("bundle.css")) // ----- concactenate all css into bundle.css
+        .pipe(csso()) // ----- minify bundle
+        .pipe(gulp.dest('public')); // ----- output destination
+});
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function () {
+    return gulp.src(['src/materialize.min.js', 'src/*.js']) // Declare source
+        .pipe(concat('bundle.js')) // Concat js files into bundle.js
+        .pipe(uglify()) // Minify the bundle
+        .pipe(gulp.dest('public/')) // Output
+});
+
+gulp.task('watch-css', function() {
+    gulp.watch('src/*.css', gulp.series('css'))
+});
+
+gulp.task('watch-js', function() {
+    gulp.watch('src/*.js', gulp.series('scripts'))
+});
+
+gulp.task('watch-build', function() {
+    gulp.watch('public/*.js', function() {
+        console.log('JS Compilation completed.')
+    })
+});
+
+gulp.task('dev', gulp.parallel('watch-js', 'watch-css', 'watch-build'))
---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+    console.log('App started baby! Gulp yeah!!!!!!!');
+    console.log('Node + Express Server Running on 3000. \nBrowserSync Proxy UI Server running on 3001.');
+  });
+  
