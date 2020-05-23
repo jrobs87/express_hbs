ee3cd048a883fbba40ef9fab60e77c4039e85777
John Robertson <tiktaalikdesign@gmail.com>
2020-05-22 23:20:13 -0400

    checkpoint

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
     // Handler when the DOM is fully loaded
-    console.log('App started! Gulp yeah!!!!!!!  Squirrel!');
-    console.log('Node + Express Server Running on 3000. \nBrowserSync Proxy UI Server running on 3001.');
+    console.log('App started - hot reload enabled.');
+
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav, {preventScrolling: true});
   });
   
